8cf2ce85d4f8d2b54b670223782a9285a3bc8461
TimmyIsCoding <thuannguyen.gamedev@gmail.com>
2025-06-12 00:41:16 +0700

    Update lesson 1.8 part 2

--- 1.8-Whitespace_and_basic_formatting/main.cpp
+++ 1.8-Whitespace_and_basic_formatting/main.cpp
@@ -37,5 +37,63 @@ BASIC FORMATTING
 + This is a mixed blessing. On one hand, it's nice to have the freedom to do whatever you like. On the other hand, many different methods of formatting C++ programs have been developed throughout the years, and you will find (sometimes significant and distracting) disagreement on which ones are best. Our basic rule of thumb is that the best styles are the ones that produce the most readble code, and provide the most consistency.
 + Here are the recommendations for basic formatting:
     1. It's fine to use either tabs or spaces for indentation (most IDEs have a setting where you can convert a tab press into the appropriate number of spaces). Developers who prefer spaces tend to do so because it ensures that code is precisely aligned as intended regardless of which editor or settings are used. Proponents of using tabs wonder why you wouldn't use the character designed to do indentation, especially as you can set the width to whatever your personal preference is. There's no right answer here -- and debating it is like arguing whether cake or pie is better. It ultimately comes dơn to personal preference.
-    
+        → Either way, we recommend you set your tabs to 4 spaces worth of indentation. Some IDEs default to 3 spaces of indentation, which is find too.
+    2. There are 2 conventional styles for function braces.
+        → Many developers prefer putting the opening curly brace on the same line as the statement:
+        int main() {
+            // Statements here
+        }
+        → The justification for this is that it reduces the amount of vertical whitespace (as you aren't devoting an entire line to an opening curly brace), so you can fit more code on a screen. This enhances code comprehension, as you don't need to scroll as much to understand what the code is doing.
+        → However, in this tutorial series, we'll use the common alternative, where the opening brace appears on its own line:
+            int main()
+            {
+                // Statements here
+            } 
+        → This enhances readability, and is less error prone since your brace paris should always be indented at the same level. If you get a compiler error due to a brace mismatch, it's very easy to see where.
+    3. Each statement within curly braces should start one tab in from the opening brace of the function it belongs to. For example:
+        int main()
+        {
+            std::cout << "Hello world!\n" // tabbed in one tab
+        }
+    4. Lines should not be too long. Typically, 80 characters has been the de facto standard for the maximum length a line should be. If a line is going to be longer, it should be split (at a reasonable spot) into multiple lines. This can be done by indenting each subsequent line with an extra tab, or if the lines are similar, by aligning it with the line above (whichever is easier to read).
+        int main()
+        {
+            // one extra indentation for the continuation line
+            std::cout << "This is a really, really, really, really, really, "
+                "really long line\n";
+
+            // text aligned with the previous line for the continuation line
+            std::cout << "This is another really, really, really, really, "
+                         "really long line\n";
+        }
+        → This makes your lines easier to read. On modern wide-screen monitors, it also allows you to place two windows with similar code side by side and compare them more easily.
+        → Best practice: Consider keeping your lines to 80 chars or less in length.
+    5. If a long line is split with an operator (e.g. << or +), the operator should be placed at the beginning of the next line, not the end of the current line.
+        std:: cout << 3 + 4
+                    + 5 + 6
+                    * 7 * 8;
+        → This helps make it clearer that subsequent lines are continuations of the previous lines, and allows you to align the operators on the left, which makes for easier reading.
+    6. Use whitespace to make your code easier to read by aligning values or comments or adding spacing between blocks of code.
+        → Hard to read:
+            cost = 57;
+            pricePerItem = 24;
+            value = 5;
+            numberOfItems = 17;
+        → Easier to read:
+            cost          = 57
+            pricePerItem  = 24    
+            value         = 5    
+            numberOfItems = 17
++ We will follow these conventions throughout this tutorial, and they will become second nature to you. As we introduce new topics to you, we will introduce new style recommendations to go with those features.
++ Ultimately, C++ gives you the power to choose whichever style you are most comfortable with, or think is best. However, we highly recommend you utilize the same style that we use for our examples. It has been battle tested by thousands of programmers over billions of lines of code, and is optimized for success.
++ One exception: if you are working in someone else's code base, adopt their styles. It's better to favor consistency than your preferences.
++ Best practice: When working in an existing project, be consistent with whatever style has already been adopted.
+------------------------------------------
+AUTOMATIC FORMATTING
++ Most modern IDEs will help you format your code as you type it in (e.g. when you create a function, the IDE will automatically indent the statements inside the function body). 
++ However, as you add or remove code, or change the IDE's default formatting, or past in a block of code that has different formatting, the formatting can get messed up. Fixing the formatting for part or all of a file can be a headache. Fortunately, modern IDEs typically contain an automatic formatting feature that will reformat either a selection (highlighted with your mouse) or an entire file.
++ Best practice: Using the automatic formatting feature is highly recommended to keep your code's formatting style consistent.
+------------------------------------------
+STYLE GUIDES
++ A style guide is a concise, opinionated document containing (sometimes arbitrary) programming conventions, formatting guidelines, and best practices. The goal of a style guide is to ensure that all developers on a project are programming in a consistent manner.
 */
