dacec2c04a8f71fda59bcfac61c13f8c0145ccfe
TimmyIsCoding <thuannguyen.gamedev@gmail.com>
2025-06-12 18:00:41 +0700

    Update lesson 1.9 part 1

---
+++ 1.9-Introduction_to_literals_and_operators/main.cpp
@@ -0,0 +1,11 @@
+/*
+------------------------------------------
+LITERALS
++ Consider the following 2 statements:
+    std::cout << "Hello world!";
+    int x{5};
+    → What are "Hello World!" and '5'? They are literals. A literal (also known as a literal constant) is a fixed value that has been inserted directly into the source code.
++ Literals and variables both have a value (and a type). Unlike a variable (whose value can be set and changed through initialization and assignment respectively), the value of a literal is fixed and cannot be changed. The literal 5 always has value 5. This is why literals are called constants.
++ To further highlight the difference between literals and variables, let's examine this short program:
+
+*/
