7a6370a2610461af255c58ac5dd0e446d9457666
TimmyIsCoding <thuannguyen.gamedev@gmail.com>
2025-06-23 18:19:51 +0700

    Update lesson 2.10 part 3

--- 2.10-Introduction_to_the_preprocessor/main.cpp
+++ 2.10-Introduction_to_the_preprocessor/main.cpp
@@ -4,5 +4,69 @@
 + Instead, prior to compilation, each code file goes through a preprocessing phase. In this phase, a programcalled the preprocessor makes various changes to the text of the code file. The preprocessor does not actually modify the original code files in any way -- rather, all changes made by the preprocessor happen either temporarily in-memory or using temporary files.
 + Historically, the preprocessor was a separate program from the compiler, but in modern compilers, the preprocessor may be built right into the compiler itself.
 + Most of what the preprocessor does is fairly uninteresting. For example, it strips out comments, and ensures each code file ends in a newline. However, the preprocessor does have one very important role: it is what processes #include directives.
-+ When the preprocessor has finished processing a code file, the result is called a translation unit. This translation unit is what is then compiled by the compiler.
++ When the preprocessor has finished processing a code file, the result is called a translation unit. This translation unit is what is then compiled by the compile.
++ The entire process of preprocessing, compiling, and linking is called translation.
+------------------------------------------
+PREPROCESSOR DIRECTIVES
++ When the preprocessor runs, it scans through the code file (from top to bottom), looking for preprocessor directives. Preprocessor directives (often just called directives) are instructions that start with a #symbol and end with a newline (NOT a semicolon). These directives tell the preprocessor to perform certain text manipulation tasks. Note that the preprocessor does not understand C++ syntax -- instead, the directives have their own syntax (which in some cases resembles C++ syntax, and in other cases, not so much).
++ The final output of the preprocessor contains no directives -- only the output of the processed directive is passed to the compiler.
++ Using directives are not preprocessor directives (and thus are not processed by the preprocessor). So while the term directive usually means a preprocessor directive, this is not always the case.
+------------------------------------------
+#include
++ You've already seen the #include directive in action (generally to #include <iostream>). When you #include a file, the preprocessor replaces the #include directive with the contants of the included file. The included contents are then preprocessed (which may result in additional #includes being preprocessed recursively), then the rest of the file is preprocessed.
++ Consider the following program:
+#include <iostream> 
+int main()
+{
+    std::cout << "Hello, world!\n";
+    return 0;
+}
+
++ When the preprocessor runs on this program, the preprocessor will replace #include <iostream> with the contants of the file named "iostream" and then preprocess the included contant and the rest of the file.
++ Each translation unit typically consists of a single code file and all header files it #includes (applied recursively, since header files can #include other header files).
+------------------------------------------
+MACRO DEFINES
++ The #define directive can be used to create a macro. In C++, a macro is a rule that defines how input text is converted into replacement output text.
++ There are 2 basic types of macros: object-like macros, and function-like macros.
++ Function-like macros act like functions, and serve a similar purpose. Their use is generally considered unsafe, and almost anything they can do can be done by a normal function.
++ Object-like macros can be defined in one of two ways:
+    #define IDENTIFIER
+    #define IDENTIFIER substitution_text
++ The top definition has no substitution text, whereas the bottom one does. Because these are preprocessor directives (not statements), note that neither form ends with a semicolon.
++ The identifier for a macro uses the same naming rules as normal identifiers: they can be letters, numbers, and underscores, cannot start with a number, and should not start with an underscore. By convention, macro names are typically all uppercase, separated by underscores.
++ Best practice: Macro names should be written in all uppercase letters, with words separated by underscores.
+------------------------------------------
+OBJECT-LIKE MACROS WITH SUBSTITUTION TEXT
++ When preprocessor encounters this directive, an association is made between the macro identifier and substitution_text. All further occurrences of the macro identifier (outside of use in other preprocessor commands) are replaced by the substitution_text.
++ Consider the following program:
+#include <iostream>
+#define MY_NAME "Timmy"
+int main()
+{
+    std::cout << "My name is: " << MY_NAME << "\n";
+    return 0;
+}
+
++ The preprocessor converts the above into the following:
+// The contents of iostream will be inserted here
+// Next is the code file 
+int main()
+{
+    std::cout << "My name is: " << "Timmy" << "\n";
+    return 0;
+}
+
++ Which, when run, prints the output: My name is: Timmy.
++ Object-like macros with substitution text were used as a way to assign names to literals. This is no longer necessary, as better methods are available in C++. Object-like macros with substitution text are now mostly seen in legacy code, and we recommend avoiding them whenever possible.
++ Best practice: Avoid macros with substitution text unless no viable alternatives exist.
+------------------------------------------
+OBJECT-LIKE MACROS WITHOUT SUBSTITUTION TEXT
++ Object-like macros can also be defined without substitution text.
++ Macros of this form work like you might expect: most further occurrences of the identifier is removed and replaced by nothing.
++ This might seem pretty useless, and it is useless for doing text substitution. However, that's not what this form of the directive is generally used for.
++ Unlike object-like macros with substitution text, macros of this form are generally considered acceptable to use.
+------------------------------------------
+CONDITIONAL COMPILATION
++ The conditional compilation preprocessor directives allow you to specify under what conditions something will or won't compile. There are quire a few different conditional compilation directives, but we'll only cover a few that are used the most often: #fdef, #ifndef, and #endif.
++ The #ifdef preprocessor directive allows the preprocessor to check whether an identifier has been previously defined via #define. If so, the code between the #ifdef and matching #endif is compiled. If not the code is ignored.
 */
