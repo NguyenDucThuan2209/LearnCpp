db9f354494aafde3c32881a147b17a63804f61ea
TimmyIsCoding <thuannguyen.gamedev@gmail.com>
2025-06-27 00:32:53 +0700

    Update lesson 2.11 part 2

--- 2.11-Header_files/main.cpp
+++ 2.11-Header_files/main.cpp
@@ -158,3 +158,71 @@ int add(int x, int y)
 // If you get a linker error about function add not being defined, make sure you've included add.cpp in your project so the definition for function add can be linked into the program.
 /*------------------------------------------*/
 // ANGLED BRACKETS VS DOUBLE QUOTES
+// You've probably curious why we angled brackets for iostream, and double quotes for add.h. It's possible that a header file with the same filename might exist in multiple directories. Our use of angled brackets vs double quotes helps give the preprocessor a clue as to where it should look for header files.
+// When we use angled brackets, we're telling the preprocessor that this is a header file we didn't write ourselves. The preprocessor will search for the header only in the directories. Our use of angled brackets vs double quotes helps give the preprocessor a clue as to where it should look for header files.
+// When we use angled brackets, we're telling the preprocessor that this is a header file we didn't write ourselves. The preprocessor will search for the header only in the directories specified by the include directories. The include directories are configured as part of your project/IDE settings/compiler settings, and typically default to the directories containing the header files that come with your compiler and/or OS. The preprocessor will not search for the header file in your project's source code directory.
+// When we use double-quotes, we're telling the preprocessor that this is a header file that we wrote. The preprocessor will first search for the header file in the current directory. If it can't find a matching header there, it will then search the include directories.
+// Use double quotes to include header files that you've written or are expected to be found in the current directory. Use angled brackets to include headers that come with your compiler, OS, or third-party libraries you've installed elsewhere on your system.
+/*------------------------------------------*/
+// WHY DOESN'T iostream HAVE A .h EXTENSION
+// Another commonly asked question is "why doesn't iostream (or any of other standard library header files) have a .h extension?". The answer is that iostream.h is a different header file than iostream! To explain requires a short history lesson.
+// When C++ was first created, all of the headers in standard library ended in a .h suffix. These headers included:
+/*
+    + Header type | Naming convention | Example | Namespace belonged
+    + C++ specific: <xxx.h> | iostream.h | Global namespace
+    + C compatibility: <xxx.h> | stddef.h | Global namespace
+*/
+// The original versions of cout and cin were declared in iostream.h in global namespace. Life was consistent, and it was good.
+// When the language was standardized by the ANSI committee, they decided to move all of the names used in the standard library into the std namespace to help avoid naming conflicts with user-declared identifiers. However, this presented a problem: if they moved all the names into the std namespace, non of the old programs (that included iostream.h) would work anymore!
+// To work around this issue, C++ introduced new header files that lack the .h extension. These new header files declare all names inside the std namespace. This way, older programs that include <iostream.h> do not need to be rewritten, and newer programs can #include <iostream>.
+// Modern C++ now contains 4 sets of header files:
+/*
+    + Header type | Naming convention | Example | Namespace belonged
+    + C++ specific (new): <xxx> | iostream | std namespace
+    + C compatibility (new): <cxxx> | stddef | std namespace
+    + C++ specific (old): <xxx.h> | iostream.h | Global namespace
+    + C compatibility (old): <xxx.h> | stddef.h | Global namespace
+*/
+// The new C compatibility headers <cxxx> may optionally declare names in the global namespace, and the old C compatibility headers <xx.h> may optionally delcare names in the std namespace. Names in these locations should be avoided, as those names may not be declared in those locations on other implementations.
+// Best practice: Use the standard library header files without the .h extension. User-defined headers should still use a .h extension.
+/*------------------------------------------*/
+// INCLUDING HEADER FILES FROM OTHER DIRECTORIES
+// Another common question involves how to include header files from other directories.
+// One (bad) way to do this is to include a relative path to the header file you want to include as part of the #include line.
+// While this will compile (assuming the files exist in those relative directories), the downside of this approach is that it requires you to reflect your directory structure in your code. If you ever update your directory structure, your code won't work anymore.
+// A better method is to tell your compiler or IDE that you have a bunch of header files in some other location, so that it will look there when it can't find them in the current directory. This can generally be done by setting an include path or search directory in your IDE project settings.
+// The nice thing about this approach is that if you ever change your directory structure, you only have to change a single compiler or IDE setting instead of every code file.
+/*------------------------------------------*/
+// HEADERS MAY INCLUDE OTHER HEADERS
+// It's common that the content of a header file will make use of something that is declared (or defined) in another header file. When this happens, the header file should #include the other header file containing the declaration (or definition) that it needs.
+/*------------------------------------------*/
+// TRANSITIVE INCLUDES 
+// When your source file #include a header file, you will also get any other header files that are #include by that header (and any header files those include, and so on). These additional header files are sometimes called transitive includes, as they're included implicity rather than explicitly rather than explicitly.
+// The content of these transitive includes are available for use in your code file. However, you generally should not rely on the content of headers that are included transitively (unless reference documentation indicates that those transitive includes are required). The implementation of header files may change over time, or be different across different systems. If that happens, your code may only compile on certain systems, or may compile now but not in the future. This is easily avoided by explicitly including all of the header files the content of your code file requires. 
+// Best practice: Each file should explicitly #include all of the header files it needs to compile. Do not rely on headers included transitively from other headers.
+// Unfortunately, there is no easy way to detect when your code file is accidentally relying on content of a header file that has been included by another header file.
+/*------------------------------------------*/ 
+// THE ORDER OF INCLUSION FOR HEADER FILES
+// If your header files are written properly and #include everything they need, the order of inclusion shouldn't matter.
+// Now consider the following scenario: let's say header A needs declarations from header B, but forgets to include it. In our code file, if we include header B before header A, our code will still compile! This is because the compiler will compile all the declarations from B before it compiles the code from A that depends on those declarations.
+// However, if we include header A first, then the compiler will complain because the code from A will be compiled before the compiler has been declarations from B. This is actually preferable, because the error has bene surfaced, and we can then fix it.
+// Best practice:
+//// To maximize the chance that missing includes will be flagged by compiler, order your #include as follows (skipping any that are not relevant)
+////// The paired header for this code
+////// Other headers from the same project
+////// 3rd party library headers
+////// Standard library headers
+//// The headers for each grouping should be sorted alphabetically (unless the documentation for a 3rd party library instructs you to do otherwise).
+// That way, if one of your user-defined headers is missing an #include for a 3rd party library or standard lirbary header, it's more likely to cause a compile error so you can fix it.
+/*------------------------------------------*/
+// HEADER FILE BEST PRACTICES
+// Here are a few more recommendations for creating and using header files:
+//// Always include header guards.
+//// Do not define variables and function in header files (for now).
+//// Give a header file the same name as the source file it's associated with.
+//// Each header file should have a specific job, and be as independent as possible.
+//// Be mindful of which headers you need to explicitly include for the functionality that you are using in your code files, to avoid inadvertent transitive includes.
+//// A header file should #include any other headers containing functionality it needs. Such a header should compile successfully when #include into a .cpp file by itself.
+//// Only #include what you need (don't include everything just because you can).
+//// Do not #include .cpp files.
+//// Prefer putting documentation on what something does or how to use it in the header. It's more likely to be seen there. Documentation describing how something works should remain in the source files.
