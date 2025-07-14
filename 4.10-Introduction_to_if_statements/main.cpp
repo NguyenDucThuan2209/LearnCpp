4102e5214738eb13749f2b1982fd4d7ec02e81cd
timmy <thuannguyen.gamedev@gmail.com>
2025-07-14 23:46:58 +0700

    Update lesson 4.10 part 1

---
+++ 4.10-Introduction_to_if_statements/main.cpp
@@ -0,0 +1,17 @@
+// Consider a case where you are going to go to the market, and your roommate tells you, "if they have straw berries on sale, buy some". This is a conditional statement, meaning that you will execute some action only if the condition is true.
+// Such conditions are common in programming, as they allow us to implement conditional behavior into our programs. The simplest kind of conditional statement in C++ is called an if statement. An if statement allows us to execute one or more lines of code only if some condition is true.
+// A condition (also called a conditional expression) is an expression that evaluates to a Boolean value.
+// If the condition of an if statement evaluates to Boolean value true, then true_statement is executed. If the condition instead evaluates to Boolean value false, the true_statement is skipped.
+/*------------------------------------------*/
+// If-else
+// We can use an alternative form of the if statement called if-else. 
+// If the condition evaluates to Boolean true, true_statement executes. Otherwise false_statement executes.
+/*------------------------------------------*/
+// CHAINING IF STATEMENTS
+// Sometimes we want to check if several things are true or false in sequence. We can do so by chaining an if-statement (or if-else) to a prior if-else.
+// Note that you can chain if statements as many times as you ahve conditions you want to evaluate. 
+/*------------------------------------------*/
+// BOOLEAN RETURN VALUES AND IF STATEMENTS
+/*------------------------------------------*/
+// NON-BOOLEAN CONDITIONALS
+// 
